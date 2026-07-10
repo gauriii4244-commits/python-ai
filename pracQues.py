3013b2bfc8c63c83da50bc03a52dd7d3b56e3601
Samruddhi Sharad <gauriii4244@gmail.com>
2026-07-10 22:37:01 +0530

    practice questions - sum, area, average, comparison using input

---
+++ pracQues.py
@@ -0,0 +1,21 @@
+#Sum of the two numbers
+a = int(input("Enter the first value: "))
+b = int(input("Enter the second value: "))
+total = a + b
+print("The sum of the given number is :", total)
+
+
+#Area of the square
+area = int(input("Enter the side of the square:"))
+print("The area of the square is :", area**2)
+
+#Two floating point number and their average
+a = float(input("Enter the first num: "))
+b = float(input("Enter the second num: "))
+average = (a + b)/2
+print(average)
+
+#print the num is greater or equal to b 
+a = int(input("Enter first value: "))
+b = int(input("Enter second value: "))
+print(a>=b)
