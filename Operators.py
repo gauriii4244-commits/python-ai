38474c1bb50b60ea402bf85658570510699b4f96
Samruddhi Sharad <gauriii4244@gmail.com>
2026-07-10 15:38:39 +0530

    practice arithmetic, relational, assignment, and logical operators

---
+++ Operators.py
@@ -0,0 +1,39 @@
+#Operators 
+#1.Arithmetic Operators
+a = 5
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+
+#2.Relational Operator
+a = 20
+b = 70
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)
+
+#3.Assignment Operator
+num = 10
+num += 10
+num -= 10
+num *= 10
+num /= 10
+num **= 10
+num %= 10
+print(num)
+
+#3.Logical operator
+
+a = 60
+b = 20
+print(not False)
+print(not True)
+print((a==b and a>b))
+
