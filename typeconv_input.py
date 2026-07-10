1507e424010e137a90f5c16ba3f35150c5da3c6b
Samruddhi Sharad <gauriii4244@gmail.com>
2026-07-10 16:05:24 +0530

    practice type conversion and input function

---
+++ typeconv_input.py
@@ -0,0 +1,20 @@
+a = float(5.36)
+b = 4 
+sum = a + b
+print(type(sum)) 
+
+num = int("2")
+print(type(num))
+
+
+#Input in python 
+name  = input("Enter Your name:")
+print("Your name is:",name)
+
+age = (int(input("Enter your age:")))
+print(type(age))
+
+
+#input() always returns a string, no matter what you type.
+x = input("Enter a number: ")
+print(type(x))
