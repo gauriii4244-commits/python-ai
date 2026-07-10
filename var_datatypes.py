be879982cc157bba321925c51299896ed422d3bc
Samruddhi Sharad <gauriii4244@gmail.com>
2026-07-10 13:50:38 +0530

    Variables and datatypes

---
+++ var_datatypes.py
@@ -0,0 +1,14 @@
+name = "Samruddhi"
+age = 19
+isstudent = True
+score = None
+
+print(name)
+print(age)
+print(isstudent)
+print(score)
+
+print(type(name))
+print(type(age))
+print(type(isstudent))
+print(type(score))
