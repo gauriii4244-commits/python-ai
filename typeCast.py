a072b1bd31e84aee39ad602ef75be800b3da7b5f
Samruddhi Sharad <gauriii4244@gmail.com>
2026-07-10 16:07:21 +0530

    Type casting

---
+++ typeCast.py
@@ -0,0 +1,7 @@
+a = int(5.36)
+b = 4 
+sum = a + b
+print(type(sum)) 
+
+num = int("2")
+print(type(num))
--- type_converse.py
+++
@@ -1,7 +0,0 @@
-# a = int(5.36)
-# b = 4 
-# sum = a + b
-# print(type(sum)) 
-
-num = int("2")
-print(type(num))
