9f589f9cfd5c35820fa77c65f8df8e9cda358cb7
Samruddhi Sharad <gauriii4244@gmail.com>
2026-07-11 20:17:31 +0530

    practice string concatenation, len, and escape characters

---
+++ string.py
@@ -0,0 +1,12 @@
+
+#string concatenation
+
+str1 = "Samruddhi"
+print(len(str1))
+str2 = "Bhosale"
+print(len(str2))
+
+print(str1 + " " + str2)
+text = "I am learning the most loved language.\nThis language is called PYTHON."
+print(text) 
+print(len(text))
