8546b70457cd418f180555e96833f782ee4a20f7
Samruddhi Sharad <gauriii4244@gmail.com>
2026-07-17 22:11:52 +0530

    lists practice - complete with all fixes (average, slicing, append pattern)

---
+++ list_methods.py
@@ -0,0 +1,66 @@
+# nums = [1,9,6,8,7]
+# nums.append(2)
+# print(nums)
+
+# nums.sort()
+# print(nums)
+
+# nums.sort(reverse=True)
+# print(nums)
+
+# nums.reverse()
+# print(nums)
+
+# nums.insert(7,10)
+# print(nums)
+
+# nums.remove(9)
+# print(nums)
+
+# nums.pop()
+# print(nums)
+
+#practice questions
+numbers = [3, 7, 2, 9, 4]
+numbers.sort(reverse=True)
+print(numbers)
+
+average = sum(numbers)/ len(numbers)
+print(average)
+
+color = ["red", "blue", "green"]
+color.insert(1,"Yellow")
+print(color)
+
+list1 = [1,2,3]
+list2 = [4,5,6]
+new = list1 + list2
+print(new)
+ 
+num = [1,4,5,1,1,8,1,2,1]
+
+print(num.count(1))
+
+lst =["a","b","c","d","e"]
+print(lst[1:-1]) 
+
+
+list5 = []
+list5.append(4)
+print(list5)
+list5.append(3)
+print(list5)
+list5.append(2)
+print(list5)
+
+names = ["Aarya","Gauri","Girija","Sneha","Anjali"]
+print(names.index("Gauri"))
+
+nums = [5, 3, 8, 1, 9]
+print(nums.pop())
+print(nums)
+
+new = ["Newyork","Italy","Dubai","London","Los Angeles"]
+print(new)
+new.clear()
+print(new)
