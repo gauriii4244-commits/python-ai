4a6bf057ae063ec11464e01257e9273aaadddee2
Samruddhi Sharad <gauriii4244@gmail.com>
2026-07-17 15:45:39 +0530

    practice lists - indexing, methods, functions, slicing, sort

---
+++ list.py
@@ -0,0 +1,43 @@
+# marks = [95.4,85.2,74.2,58.4]
+# print(marks)
+# print(type(marks))
+# print(marks[3])
+# print(len(marks))
+
+# #can store different types of data
+# student = ["Aarya", 91.80 , 19 , "Baramati"]
+# print(student)
+# student[1]=92.80
+# print(student)
+# print(student[5]) #list index is out of range
+
+#practice questions
+foods =["Momos","Noodles","Pani-puri","Fried-rice","Pav-bhaji"]
+print(foods)
+print(foods[0])
+print(foods[-1])
+
+numbers = [10, 20, 30, 40, 50]
+print(sum(numbers))
+numbers.append(60)
+print(numbers)
+numbers.remove(numbers[1])
+print(numbers)
+
+num = [90,50,58,22,10]
+print(max(num))
+print(min(num))
+
+fruit_list = ["Apple","Banana","Mango","Cherry","Grapes"]
+print("Apple" in fruit_list)
+print("Kiwi" in fruit_list)
+
+print(fruit_list[::-1])
+
+nums = [1,2,3,4,5]
+print(nums[0:3])
+
+num1 = [15,5,10,25,20]
+num1.sort()
+print(num1)
+
